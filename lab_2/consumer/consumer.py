159ed7e4e1b1ce78c8ab4bd91db9c62eb1566a8f
DujeStolfa <duje.stolfa626@gmail.com>
2023-12-10 15:08:38 +0100

    Add lab_1 files

---
+++ lab_2/consumer/consumer.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from azure.eventhub.aio import EventHubConsumerClient
+
+EVENT_HUB_CONNECTION_STR = "Endpoint=sb://mostovi.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ukCi1jQV55q6TksBn9/gwYtOFSzCdu3MV+AEhNDXLX8="
+EVENT_HUB_NAME = "mostovi-hub"
+
+
+async def on_event(partition_context, event):
+    print(
+        'Received the event: "{}" from the partition with ID: "{}"'.format(
+            event.body_as_str(encoding="UTF-8"), partition_context.partition_id
+        )
+    )
+
+    await partition_context.update_checkpoint(event)
+
+
+async def main():
+    client = EventHubConsumerClient.from_connection_string(
+        EVENT_HUB_CONNECTION_STR,
+        consumer_group="$Default",
+        eventhub_name=EVENT_HUB_NAME,
+    )
+    async with client:
+        await client.receive(on_event=on_event, starting_position="-1")
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    print("START")
+    loop.run_until_complete(main())
+    print("STOP")
---
+++ lab_2/producer/producer.py
@@ -0,0 +1,58 @@
+import asyncio
+import requests
+import requests.auth
+
+from azure.eventhub.aio import EventHubProducerClient
+from azure.eventhub.exceptions import EventHubError
+from azure.eventhub import EventData
+
+CONNECTION_STR = "Endpoint=sb://mostovi.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ukCi1jQV55q6TksBn9/gwYtOFSzCdu3MV+AEhNDXLX8="
+EVENTHUB_NAME = "mostovi-hub"
+
+
+async def run():
+    client_auth = requests.auth.HTTPBasicAuth(
+        "paTgAbKTO4I2qfRzcJvckQ", "qv0fRkYDKY3XDRfmQOXl7TIeupGOnA"
+    )
+    post_data = {
+        "grant_type": "password",
+        "username": "oblak-faks-labos",
+        "password": "severina123",
+    }
+    headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+
+    # Get access token
+    response = requests.post(
+        "https://www.reddit.com/api/v1/access_token",
+        auth=client_auth,
+        data=post_data,
+        headers=headers,
+    )
+    auth_resp = response.json()
+
+    # Fetch data
+    headers = {
+        "Authorization": f"bearer {auth_resp['access_token']}",
+        "User-Agent": "ChangeMeClient/0.1 by YourUsername",
+    }
+    url = "https://oauth.reddit.com/r/dataengineering/top/?limit=10&t=all"
+    response = requests.get(url, headers=headers)
+    resp_json = response.json()
+
+    producer = EventHubProducerClient.from_connection_string(
+        conn_str=CONNECTION_STR, eventhub_name=EVENTHUB_NAME
+    )
+
+    async with producer:
+        event_data_batch = await producer.create_batch()
+
+        for post in resp_json["data"]["children"]:
+            event_data_batch.add(EventData(str(post)))
+
+        await producer.send_batch(event_data_batch)
+
+
+print("HEEJ")
+asyncio.run(run())
+
+print("GOTOV")
